c01671c911e791fe7219bd428b211c7063222a10
oggdoge <userg107@yandex.ru>
2016-08-19 17:57:06 +0300

    New bugs were created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
-/**
- * Created by roman on 17.08.16.
- */
-public class Main {
+import org.telegram.telegrambots.TelegramApiException;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class Main extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Main());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "Javaholic";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "183358065:AAEVJiay8q1FabRQobB5B2x1jbmQryl5dc";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            if (message.getText().equals("/help"))
+                sendMsg(message, "Hi! Can I do smth for u?");
+            else
+                sendMsg(message, "I don`t know how to answer to ur question");
+        }
+    }
+
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplayToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ src/TelegramJavaholic.java
@@ -0,0 +1,7 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.TelegramApiException;
+
+public class TelegramJavaholic {
+
+}
